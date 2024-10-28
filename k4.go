64dcb9919e0113e2054fd3ff5230c57e7ac82ff7
Alex Gaetano Padula <apadula@cursusdb.com>
2024-10-28 00:48:29 -0400

    k4 main structures

--- k4.go
+++ k4.go
@@ -30,7 +30,78 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package k4
 
+import (
+	"k4/pager"
+	"k4/skiplist"
+	"os"
+	"sync"
+	"time"
+)
+
 const SSTABLE_EXTENSION = ".sst"     // The SSTable file extension
 const LOG_EXTENSION = ".log"         // The log file extension
 const WAL_EXTENSION = ".wal"         // The write ahead log file extension
 const TOMBSTONE_VALUE = "$tombstone" // The tombstone value
+
+// k4 is the main structure for the k4 database
+type K4 struct {
+	sstables               []*SSTable         // in memory sstables.  We just keep the opened file descriptors
+	sstablesLock           *sync.RWMutex      // read write lock for sstables
+	memtable               *skiplist.SkipList // in memory memtable (skip list)
+	memtableLock           *sync.RWMutex      // read write lock for memtable
+	memtableFlushThreshold int                // in bytes
+	compactionInterval     int                // in seconds, pairs up sstables and merges them
+	directory              string             // the directory where the database files are stored
+	lastCompaction         time.Time          // the last time a compaction was run
+	transactions           []*Transaction     // in memory transactions
+	transactionsLock       *sync.RWMutex      // read write lock for transactions
+	logging                bool               // whether or not to log to the log file
+	logFile                *os.File           // the log file
+	wal                    *pager.Pager       // the write ahead log
+	wg                     *sync.WaitGroup    // wait group for the wal
+	walQueue               []*Operation       // the write ahead log queue
+	walQueueLock           *sync.Mutex        // mutex for the wal queue
+	exit                   chan struct{}      // channel to signal the background wal routine to exit
+}
+
+// SSTable is the structure for the SSTable files
+type SSTable struct {
+	pager *pager.Pager  // the pager for the sstable file
+	lock  *sync.RWMutex // read write lock for the sstable
+}
+
+// Transaction is the structure for the transactions
+type Transaction struct {
+	id  int64        // Unique identifier for the transaction
+	ops []*Operation // List of operations in the transaction
+}
+
+// Operation Used for transaction operations and WAL
+type Operation struct {
+	op       OPR_CODE   // Operation code
+	key      []byte     // Key for the operation
+	value    []byte     // Value for the operation
+	rollback *Operation // Pointer to the operation that will undo this operation
+}
+
+type OPR_CODE int // Operation code
+
+const (
+	PUT OPR_CODE = iota
+	DELETE
+	GET
+)
+
+// SSTableIterator is the structure for the SSTable iterator
+type SSTableIterator struct {
+	pager       *pager.Pager // the pager for the sstable file
+	currentPage int          // the current page
+	lastPage    int          // the last page in the sstable
+}
+
+// WALIterator is the structure for the WAL iterator
+type WALIterator struct {
+	pager       *pager.Pager // the pager for the wal file
+	currentPage int          // the current page
+	lastPage    int          // the last page in the wal
+}
