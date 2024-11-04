3bc6f87a1bfd75d09f6a84d8c3cfdb0c46a67ca7
Alex Gaetano Padula <me@alexpadula.com>
2024-11-04 02:33:24 -0500

    python ffi: start of setup.py

---
+++ ffi/python/setup.py
@@ -0,0 +1,9 @@
+# K4 setup.py
+
+from setuptool import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Python FFI for the K4 storage engine'
+LONG_DESCRIPTION = 'Python FFI for the K4 storage engine'
+
+
