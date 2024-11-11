c2f9574e40a7699c4e6725bf8ae9b1eb2b35a926
Alex Gaetano Padula <me@alexpadula.com>
2024-11-11 05:41:46 -0500

    bench: more detailed anc cleaner output for bench.go

--- bench/bench.go
+++ bench/bench.go
@@ -16,7 +16,8 @@ const (
 	NUM_THREADS = 4
 )
 
-func benchmarkK4() {
+func benchmarkK4(thread int) {
+
 	db, err := k4.Open(DB_PATH, (1024*1024)*256, 3600, false, false)
 	if err != nil {
 		log.Fatalf("Error opening K4 database: %v", err)
@@ -36,7 +37,7 @@ func benchmarkK4() {
 		}
 	}
 	cpuTimeUsed := time.Since(start).Seconds()
-	fmt.Printf("K4 Put: %f seconds\n", cpuTimeUsed)
+	fmt.Printf("K4 Put(%d): %f seconds\n", thread, cpuTimeUsed)
 
 	// Benchmark Get
 	start = time.Now()
@@ -47,7 +48,7 @@ func benchmarkK4() {
 		}
 	}
 	cpuTimeUsed = time.Since(start).Seconds()
-	fmt.Printf("K4 Get: %f seconds\n", cpuTimeUsed)
+	fmt.Printf("K4 Get(%d): %f seconds\n", thread, cpuTimeUsed)
 
 	// Benchmark Delete
 	start = time.Now()
@@ -58,7 +59,7 @@ func benchmarkK4() {
 		}
 	}
 	cpuTimeUsed = time.Since(start).Seconds()
-	fmt.Printf("K4 Delete: %f seconds\n", cpuTimeUsed)
+	fmt.Printf("K4 Delete (%d): %f seconds\n", thread, cpuTimeUsed)
 
 	os.RemoveAll(DB_PATH)
 }
@@ -120,7 +121,8 @@ func benchmarkK4Concurrent() {
 	for i := 0; i < NUM_THREADS; i++ {
 		go func() {
 			defer wg.Done()
-			benchmarkK4()
+
+			benchmarkK4(i)
 		}()
 	}
 
@@ -128,7 +130,19 @@ func benchmarkK4Concurrent() {
 }
 
 func main() {
-	benchmarkK4()
+
+	fmt.Println("Benchmarker started with the set parameters:")
+	fmt.Printf("Number of operations: %d\n", NUM_OPS)
+	fmt.Printf("Number of threads: %d\n", NUM_THREADS)
+
+	fmt.Println()
+
+	fmt.Println("Benchmarking K4 non concurrent operations")
+	benchmarkK4(-1)
+	fmt.Println()
+	fmt.Println("Benchmarking K4 random operations")
 	benchmarkK4Random()
+	fmt.Println()
+	fmt.Println("Benchmarking K4 concurrent operations")
 	benchmarkK4Concurrent()
 }
